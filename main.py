f41ded05313f08331d41e8ca1fc238a75555e2f1
Zhygaliuk <mik19122002@gmail.com>
2021-02-22 11:07:17 +0200

    Change 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-class Hotel():
-    static = 0
+class Hotel:
+    count = 0
 
     def __init__(self, name='', visitor_of_year=0, number_of_rooms=0):
         self.name = name
         self.visitor = visitor_of_year
         self.number = number_of_rooms
+        Hotel.count = Hotel.count+1
 
     def __str__(self):
         return f"name:{self.name} visitor:{self.visitor} number:{self.number}"
@@ -13,19 +14,16 @@ class Hotel():
         print("викликаэмо деструктор")
 
     @staticmethod
-    def staticmethod(static):
-        return f"static={static}"
+    def staticmethod():
+        return Hotel.count
 
 
-Lisha_1=Hotel("Lisha_1",10,10)
-print(Lisha_1.__str__())
+Lisha_1 = Hotel("Lisha_1", 10, 10)
+Lisha_2 = Hotel("Lisha_2", 6, 7)
+Lisha_3 = Hotel("Lisha_3", 15, 15)
+Lisha_4 = Hotel("Lisha_4", 11, 12)
 
-Lisha_2=Hotel("Lisha_2",16,17)
-print(Lisha_2.__str__())
+print(Lisha_1, Lisha_2, Lisha_3, Lisha_4)
 
-Lisha_3=Hotel("Lisha_3",15,15)
-print(Lisha_3.__str__())
-
-Lisha_4=Hotel("Lisha_4",11,12)
-print(Lisha_4.__str__())
+print(f"Count={Hotel.getcount}")
 
